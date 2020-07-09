0a0e6f4da84ebcaea1d62a639242ea8f3f0b621a
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-07-08 22:58:38 -0400

    splitting into a method and putting into a directory

---
+++ HelloWorld/Log.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Tyler Clark on 7/8/20.
+//
+#include <iostream>
+
+void Log(const char* message){
+    std::cout << message << std::endl;
+}
---
+++ HelloWorld/hello_world.cpp
@@ -0,0 +1,10 @@
+#include "Log.cpp"
+
+//declaring the function is not working for me
+//void Log(const char* message);
+
+int main() {
+    Log("Hello, World!");
+    //return 0; not entirely necessary
+    //std::cin.get();
+}
