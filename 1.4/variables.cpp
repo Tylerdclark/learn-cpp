7c98bd5d40455ccdf13bb86f9a90dcfd835f9145
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-08-06 00:35:34 -0400

    Working with vars

---
+++ 1.4/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int add_mult(int arg1, int arg2)
+{
+    return (arg1 + arg2) * (arg1 +arg2);
+}
+
+int main(int argc, char const *argv[])
+{
+    int x = 5;
+    int y (5);
+    int z;
+    std::cout << add_mult(4,4) << std::endl;
+    int obj{0};
+}
