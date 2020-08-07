1fac770639c6a1604068c7da355b2f1703c06b99
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-08-06 21:08:04 -0400

    Variable scratch file

--- 1.4/variables.cpp
+++ 1.4/variables.cpp
@@ -4,12 +4,18 @@ int add_mult(int arg1, int arg2)
 {
     return (arg1 + arg2) * (arg1 +arg2);
 }
-
+// argc is the number of arguments to follow and argv[] is the vector that
+// would hold the arguments if this would be a commandline program
 int main(int argc, char const *argv[])
 {
-    int x = 5;
-    int y (5);
-    int z;
     std::cout << add_mult(4,4) << std::endl;
-    int obj{0};
+    int value1{0}; //direct brace initialization to zero
+    int value2(4.5); //direct initialization, will truncate to 4
+    /* int value3{5.5}; direct brace initialization, will not
+     compile; no narrowing conversions allowed*/
+    int value4{5}; // this is fine
+
+    std::cout << value1 << std::endl;
+    std::cout << value2 << std::endl;
+    std::cout << value4 << std::endl;
 }
