f90e4acc34be2799644bc65c96154d6207f08478
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-08-07 23:17:08 -0400

    Messing with iostream

--- 1.5/iostream.cpp
+++ 1.5/iostream.cpp
@@ -1,5 +1,18 @@
 //
 // Created by Tyler Clark on 8/6/20.
 //
+#include <iostream> // <-- All about it
 
-#include "iostream.h"
+int main(){
+
+    std::cout << "inserting the string into the predefined variable std::cout \n";
+    std::cout << "printing with std::endl redundantly flushes" << std::endl;
+    std::cout << "Therefore using '\\n' is more efficient. \n";
+
+    std::string userInput{}; //initialize the string
+    std::cout << "Enter your name ";
+    std::cin >> userInput;
+    std::cout << "I got your name thanks to std::cin, " << userInput << '.';
+
+    return 0; //if not explicated stated, will return 0
+}
